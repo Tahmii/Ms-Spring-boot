913bfd3aab9dc056511923287ef83ce6328127c9
Tahmii <tahmi.ma222@gmail.com>
2023-08-21 20:49:14 +0200

    first commit

---
+++ src/main/java/com/ms/sb/MsSbApplication.java
@@ -0,0 +1,13 @@
+package com.ms.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  MsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSbApplication.class, args);
+	}
+
+}
